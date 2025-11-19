52317e220403105a1cb3202ca2892c3cb8bfdf91
EasilyAI <omer@easilyai.biz>
2025-11-19 02:22:18 +0200

    Refactor type exports: Replace index.js with index.ts for TypeScript compatibility and maintainability.

--- WebApp/src/types/index.js
+++
@@ -1,5 +0,0 @@
-// Export all types from a single module for easier imports
-export * from './products.ts';
-export * from './quotataions.ts';
-export * from './catalogProduct.ts';    
-export * from './files.ts';
---
+++ WebApp/src/types/index.ts
@@ -0,0 +1,6 @@
+// Export all types from a single module for easier imports
+export * from './products';
+export * from './quotataions';
+export * from './catalogProduct';    
+export * from './files';
+
